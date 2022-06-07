9e2b21bf64a56e9ba9d8016a9bc4103c2790041a
brandonhsz <brandonhszzz@gmail.com>
2022-06-07 02:36:18 -0500

    README added like a index.html

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TicketsModule } from './tickets/tickets.module';
 
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     TicketsModule,
